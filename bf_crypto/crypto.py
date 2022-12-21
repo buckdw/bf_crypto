daedfa29550db0fdb1e8f8b81b3c395a531c8e90
Diederick de Buck <diederick.de.buck@gmail.com>
2022-12-21 17:16:08 +0100

    dddd

--- bf_crypto/crypto.py
+++ bf_crypto/crypto.py
@@ -3,7 +3,7 @@
 #
 
 __author__ = 'Diederick de Buck'
-__version__ = '1.0'
+__version__ = '3.0.0'
 __date__ = "$Jan 29, 2019 10:02:13 PM$"
 __maintainer__ = 'Diederick de Buck'
 __email__ = 'diederick.de.buck@esolutionssys.com'
@@ -11,10 +11,16 @@ __email__ = 'diederick.de.buck@esolutionssys.com'
 from fernet import Fernet
 
 def encrypt(plaintext, fernetkey):
-    fernet = Fernet(fernetkey)
-    return fernet.encrypt(bytes(plaintext, "utf-8")).decode("utf-8")
+    try:
+        fernet = Fernet(fernetkey)
+        return fernet.encrypt(bytes(plaintext, "utf-8")).decode("utf-8")
+    except:
+        return None
 
 
 def decrypt(cypher, fernetkey):
-    fernet = Fernet(fernetkey)
-    return fernet.decrypt(bytes(cypher, "utf-8")).decode("utf-8")
+    try:
+        fernet = Fernet(fernetkey)
+        return fernet.decrypt(bytes(cypher, "utf-8")).decode("utf-8")
+    except:
+        return None
